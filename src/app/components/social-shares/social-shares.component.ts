df42e881b35b970a2e90139d2c3dc4a8e624050c
Darshan Mhatre <darshan2531mhatre@gmail.com>
2020-01-10 18:56:38 +0530

    social shares

--- src/app/components/social-shares/social-shares.component.ts
+++ src/app/components/social-shares/social-shares.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ReviewHttpService } from 'src/app/services/review-http.service';
 
 @Component({
   selector: 'app-social-shares',
@@ -6,10 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./social-shares.component.scss']
 })
 export class SocialSharesComponent implements OnInit {
+  constructor(public _reviewHttp: ReviewHttpService) {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
