ea5ede9000c550eea25a29375953c004a01f3474
Varun Sukheja <varun.sukheja@myglamm.com>
2020-01-10 12:49:52 +0530

    added md-bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,16 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 
 import { AngularFullpageModule } from "@fullpage/angular-fullpage";
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AngularFullpageModule],
+  imports: [BrowserModule,
+    AngularFullpageModule,
+    MDBBootstrapModule.forRoot()
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
